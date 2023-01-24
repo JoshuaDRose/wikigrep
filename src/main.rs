a194c77d5b938a49498e165c35e81cddb7e73ac9
Joshua Rose <joshuarose099@gmail.com>
2023-01-24 23:08:20 +1100

    Mofidied dependency list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use http::{Request, Response};
+use http::client::Client;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let request = Request::get("http://httpbin.org/ip").body(())?;
+    let response = client.send(request)?;
+    println!("{:?}", response);
+    Ok(())
 }
