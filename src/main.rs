aed0c09f32076602134e29d25fe159e0dac96b8f
Joshua Rose <joshuarose099@gmail.com>
2023-01-24 23:13:01 +1100

    Added std::env::current_dir

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use http::{Request, Response};
-use http::client::Client;
+use std::env::current_dir;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let request = Request::get("http://httpbin.org/ip").body(())?;
-    let response = client.send(request)?;
-    println!("{:?}", response);
-    Ok(())
+fn main() {
+    println!("{} {}", 0, 0);
 }
